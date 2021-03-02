e16b0db683df5a1e5095449ff1072b66beb77bb7
Matheus Botelho <matheusbotelho281@gmail.com>
2021-03-02 11:45:53 -0300

    Dia 2 | 2

--- src/components/HeaderMenu/index.js
+++ src/components/HeaderMenu/index.js
@@ -1,13 +1,55 @@
-import { MenuButton, Wrapper } from "./styles"
+import {useState} from 'react'
+import { MenuButton, MenuItems, WrapperMenu } from "./styles"
 
 export default function Menu(){
+
+  const[isMenuOpened, setIsMenuOpened] = useState(false)
+  const [linkSelected, setLinkSelected] = useState('home')
+
+  function choosePage(page){
+    setLinkSelected(page)
+  }
+
+  function ToggleMenu(){
+    setIsMenuOpened(!isMenuOpened)
+  }
+
+  const Selected = {
+    background: '#bf8760',
+    color: '#fff'
+  }
+
   return(
     <div>
-      <Wrapper>
-        <MenuButton>
-          <img src='../media/menu.png' alt='Menu' />
+      <WrapperMenu>
+        <MenuButton onClick={ToggleMenu}>
+          <img src={isMenuOpened ? './media/close.png' : './media/menu.png'} alt='Menu' />
         </MenuButton>
-      </Wrapper>
+        <MenuItems
+         height={isMenuOpened ? '17.5rem' : '0'}
+         bottom={isMenuOpened ? '-17.5rem' : '0'}>
+        <div onClick={() => choosePage('home')}
+        style={linkSelected == 'home' ? Selected : null}>
+          <a>Home</a>
+        </div>
+        <div onClick={() => choosePage('sobre')}
+        style={linkSelected == 'sobre' ? Selected : null}>
+          <a>Sobre</a>
+        </div>
+        <div onClick={() => choosePage('contato')}
+        style={linkSelected == 'contato' ? Selected : null}>
+          <a>Contato</a>
+        </div>
+        <div onClick={() => choosePage('portifolio')}
+        style={linkSelected == 'portifolio' ? Selected : null}>
+          <a>Portifolio</a>
+        </div>
+        <div onClick={() => choosePage('cadastrar')} 
+        style={linkSelected == 'cadastrar' ? Selected : null}>
+          <a>Cadastrar</a>
+        </div>
+        </MenuItems>
+      </WrapperMenu>
     </div>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,11 @@ import Header from '../components/Header'
 
 export default function Home() {
   return (
-    <div><Header /></div>
-  
+    <div>
+      <Header /> 
+      <div>
+        aoba
+      </div>
+    </div>
   )
 }
